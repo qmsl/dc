00d1f758bc9d4a4e6a1f33891ed9bdc5135331df
qmsl <q418358795>
2020-10-21 21:13:09 +0800

    init

--- src/main/java/com/ty/dc/controller/AuthController.java
+++ src/main/java/com/ty/dc/controller/AuthController.java
@@ -4,16 +4,13 @@ import com.ty.dc.base.BaseController;
 import com.ty.dc.interceptor.AuthIgnore;
 import com.ty.dc.service.IUserService;
 import com.ty.dc.utils.Global;
-import com.ty.dc.utils.StringUtils;
 import com.ty.dc.weixin.WxCpConfiguration;
 import me.chanjar.weixin.common.api.WxConsts;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.cp.api.WxCpService;
 import me.chanjar.weixin.cp.bean.WxCpOauth2UserInfo;
-import me.chanjar.weixin.cp.bean.WxCpUserDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.Cookie;
@@ -42,36 +39,37 @@ public class AuthController extends BaseController {
 
         Cookie cookie1 = new Cookie("uid", "1");
         cookie1.setPath("/");
-        cookie1.setMaxAge(-1);
+        cookie1.setMaxAge(10);
         getResponse().addCookie(cookie1);
 
         Cookie cookie = new Cookie("state", state);
         cookie.setPath("/");
-        cookie.setMaxAge(-1);
+        cookie.setMaxAge(10);
         getResponse().addCookie(cookie);
         return "auth";
+
+        //return "auth?uid=111&state="+state+"&_t="+System.currentTimeMillis();
+
+        /*switch (state) {
+            case "1":
+                return "/#/auth?sid=111&action=list";
+            case "2":
+                return "/#/auth?sid=111&action=scan";
+            case "3":
+                Cookie uid = new Cookie("uid", "111");
+                uid.setPath("/");
+                uid.setMaxAge(-1);
+                getResponse().addCookie(uid);
+                return "/order/admin/index.html";
+            default:
+                return "/404.html";
+        }*/
     }
 
-    @RequestMapping("bindex")
-    @AuthIgnore
-    public String bndex() {
-
-        Cookie cookie1 = new Cookie("uid", "z4suh8h");
-        cookie1.setPath("/");
-        cookie1.setMaxAge(-1);
-        getResponse().addCookie(cookie1);
-
-        Cookie cookie = new Cookie("state", "3");
-        cookie.setPath("/");
-        cookie.setMaxAge(-1);
-        getResponse().addCookie(cookie);
-        return "auth";
-    }
 
     @RequestMapping("auth")
     @AuthIgnore
-    public String auth(@CookieValue String state) {
-
+    public String auth(String state) {
         WxCpService wxCpService = WxCpConfiguration.getCpService(1000033);
         String url = wxCpService.getOauth2Service().buildAuthorizationUrl(Global.getRedirecturl() + redirect_url, state, WxConsts.OAuth2Scope.SNSAPI_USERINFO);
         return "redirect:" + url;
@@ -85,7 +83,7 @@ public class AuthController extends BaseController {
 
         Cookie cookieUid = new Cookie("uid", userInfo.getUserId());
         cookieUid.setPath("/");
-        cookieUid.setMaxAge(60 * 60 * 24 * 30 * 3);
+        cookieUid.setMaxAge(-1);
         getResponse().addCookie(cookieUid);
 
         Cookie cookieState = new Cookie("state", state);
@@ -97,6 +95,5 @@ public class AuthController extends BaseController {
 
         return "auth";
     }
-
 
 }
